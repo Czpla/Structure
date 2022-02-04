658465571eb5e4add23f50016d83d3c1a00e58cf
czpla <edu.czpla@gmail.com>
2022-02-04 12:03:49 -0300

    Add version 1.0.2
    Settings initial finish
    
    add type eol settings
    
    remove package-lock.json and add to .gitignore
    
    add package.lock.json to .gitignore
    
    add settings to prettier
    
    change project name
    
    Create function generateArray
    
    Create class searchChar
    
    Create class searchBinRecursive
    
    Create strucute for test project
    
    Add readme.md

---
+++ README.md
@@ -0,0 +1,44 @@
+<p align="center">
+  <img src="https://img.shields.io/github/stars/Czpla/structure?label=stars&message=MIT&color=000000&labelColor=0c6dff" alt="Stars">
+  <img src="https://img.shields.io/github/forks/Czpla/structure?label=forks&message=MIT&color=000000&labelColor=0c6dff" alt="Forks">     
+  <img  src="https://img.shields.io/static/v1?label=license&message=MIT&color=000000&labelColor=0c6dff" alt="License">
+</p>
+
+---
+
+<h2 align="center">Sobre 📖</h2>
+
+Projeto de estudo sobre estrutura de dados, onde foram implementados busca sequêncial e busca binária.
+
+---
+
+## 💻 Objetivo
+
+-   Analisar como funciona buscas em vetores e arrays
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias
+
+-   TypeScript
+-   Node.js
+-   Prettier
+-   eslint
+
+## 🎯 Instalação da aplicação
+
+1.  Utilize um terminal da sua escolha e rode o seguinte comando
+
+```bash
+ git clone https://github.com/Czpla/structure.git
+```
+
+2. Rode o seguinte comando para instar as dependências
+
+```bash
+ npm i
+```
+
+---
+
+Feito com ♥ by Eduardo Czpla
---
+++ src/searchBinRecursive.ts
@@ -0,0 +1,66 @@
+import { GenerateArray } from './generateArray';
+
+interface Filter {
+    data: object;
+    result: boolean;
+    interation: number;
+}
+
+export class SearchBinRecursive {
+    private data: number[];
+    private result: Filter = { data: {}, result: false, interation: 1 };
+
+    constructor() {
+        const dataGenerated = new GenerateArray().generateRandonFloat(9999);
+        this.data = this.orderByAsc(dataGenerated);
+    }
+
+    private orderByAsc(data: number[]): number[] {
+        return data.sort((a: number, b: number) => a - b);
+    }
+
+    private searchBin(
+        float: number,
+        array: Array<string | number>,
+        interation: number,
+    ): Filter {
+        const sizeNewArray = Math.round(array.length / 2);
+        if (array.length > 1) {
+            if (array[sizeNewArray] > float) {
+                const newArray = array.slice(0, sizeNewArray);
+
+                this.searchBin(float, newArray, ++interation);
+            } else if (array[sizeNewArray] < float) {
+                const newArray = array.slice(sizeNewArray, array.length);
+
+                this.searchBin(float, newArray, ++interation);
+            } else if (array[sizeNewArray] === float) {
+                this.result = {
+                    data: this.data,
+                    result: true,
+                    interation: interation,
+                };
+            }
+        } else {
+            if (array[0] === float) {
+                this.result = {
+                    data: this.data,
+                    result: true,
+                    interation: interation,
+                };
+            } else {
+                this.result = {
+                    data: this.data,
+                    result: false,
+                    interation: interation,
+                };
+            }
+        }
+
+        return this.result;
+    }
+
+    public search(float: number): Filter {
+        return this.searchBin(float, this.data, 1);
+    }
+}
