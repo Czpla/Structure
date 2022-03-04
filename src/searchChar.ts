ce00394db6225512632da198db58c2bcfb8d7df5
czpla <edu.czpla@gmail.com>
2022-03-04 14:48:43 -0300

    Create class searchChar

---
+++ src/searchChar.ts
@@ -0,0 +1,23 @@
+import { GenerateArray } from './generateArray';
+
+interface Filter {
+    data: object;
+    result: number;
+}
+
+export class SearchChar {
+    private data: Array<string | number>;
+
+    constructor() {
+        this.data = new GenerateArray().generateRandonChars(10);
+    }
+
+    public search(char: string): Filter {
+        const result = this.data.indexOf(char);
+
+        return {
+            data: this.data,
+            result,
+        };
+    }
+}
